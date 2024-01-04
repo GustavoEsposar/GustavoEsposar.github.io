af2e1a73bd782feb29019059af439ac9ced1eef9
guesposar <guesposar@icloud.com>
2024-01-03 21:36:27 -0300

    remove text-index property and funcitonless script

--- script.js
+++ script.js
@@ -8,27 +8,3 @@ const root = document.documentElement
 root.style.setProperty('--original-viewport-height', window.innerHeight + "px");
 root.style.setProperty('--original-viewport-width', window.innerWidth + "px");
 //--------------------------------------------------------------------------
-
-/*
-buttonAbout.onclick = () => {
-    fetch('./index.html')
-        .then(response => response.text())
-        .then(html => {
-            content.innerHTML = html;
-        })
-        .catch(error => {
-            console.log('Erro ao carregar o arquivo HTML:', error);
-        });
-}
-
-buttonCertified.onclick = () => {
-    fetch('./certificados.html')
-        .then(response => response.text())
-        .then(html => {
-            content.innerHTML = html;
-        })
-        .catch(error => {
-            console.log('Erro ao carregar o arquivo HTML:', error);
-        });
-}
-*/
