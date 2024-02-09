aa3a50270d0d2e5fbd2891d0fdd4e6fcc83c27fb
guesposar <guesposar@icloud.com>
2024-02-08 22:53:14 -0300

    certificates html and css funcitonality

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 // --------- Tokens Storage --------- 
-const buttonAbout = document.querySelector('.about')
-const buttonCertified = document.querySelector('.projects')
+const buttonAbout = document.querySelector('.about');
+const buttonCertified = document.querySelector('.projects');
+const certifications_list = document.querySelectorAll('.fullscreen-img');
 //-----------------------------------
 
 // -----------  Setting a constant for viewPort Height and Width -----------
-const root = document.documentElement
-root.style.setProperty('--original-viewport-height', window.innerHeight + "px");
-root.style.setProperty('--original-viewport-width', window.innerWidth + "px");
+const root = document.documentElement;
+root.style.setProperty('--original-viewport-height', window.innerHeight + "px")
+root.style.setProperty('--original-viewport-width', window.innerWidth + "px")
 //--------------------------------------------------------------------------
+
+certifications_list.forEach( (img) => {
+    img.addEventListener('click', function () {
+        if (this.requestFullscreen) {
+            this.requestFullscreen();
+        } else if (this.webkitRequestFullscreen) { /* Safari */
+            this.webkitRequestFullscreen();
+        } else if (this.msRequestFullscreen) { /* IE11 */
+            this.msRequestFullscreen();
+        }
+    })
+})
